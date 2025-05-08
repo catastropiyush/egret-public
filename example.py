57b5321839f754fb9af73dd3298060ad26137d81
EliasLMann <eliaslmann@gmail.com>
2025-05-08 17:34:16 -0400

    add descriptor example

--- example.py
+++ example.py
@@ -1,12 +1,23 @@
 import ase.io
+import torch
 from ase.calculators.calculator import all_changes
-
 from mace.calculators import mace_off
 
-atoms = ase.io.read("static/aspirin.xyz", format="xyz")
 
+atoms = ase.io.read("static/aspirin.xyz", format="xyz")
 calculator = mace_off(model="compiled_models/EGRET_1.model", default_dtype="float64")
 
+#to predict energies and forces
 calculator.calculate(atoms, ["energy", "forces"], all_changes)
-
 print(calculator.results)
+
+#to obtain node features to use as a molecule descriptor
+full_descriptor = calculator.models[0](calculator._atoms_to_batch(atoms).to_dict())["node_feats"]
+print(f'{full_descriptor.shape=}')
+
+#to extract only the inviariant features
+invariant_descriptor = torch.cat([
+    full_descriptor[:, :192],
+    full_descriptor[:, -192:],
+], dim=1)
+print(f'{invariant_descriptor.shape=}')
